dbd4fbcb897e10cfbf7d9e79ce52c166a55dfb9c
samuelfooks <samuel.fooks@gmail.com>
2022-12-21 14:03:02 +0100

    clean up

--- dashboard/interactive_cla_finder.py
+++ dashboard/interactive_cla_finder.py
@@ -1,14 +1,10 @@
 import datetime
-
 import numpy as np
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder, JsCode,GridUpdateMode
-
-
 now = int(datetime.datetime.now().timestamp())
 start_ts = now - 3 * 30 * 24 * 60 * 60
-
 # @st.cache(allow_output_mutation=True)
 # def make_data():
 #     df = pd.DataFrame(
@@ -38,14 +34,14 @@ start_ts = now - 3 * 30 * 24 * 60 * 60
 #         "datetime",
 #         df.timestamp.apply(lambda ts: datetime.datetime.fromtimestamp(ts)),
 #     )
-
 #     return df.sort_values("timestamp").drop("timestamp", axis=1)
+
 cla_df = pd.read_csv(
    # "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
    "./csv_data/final.csv"
-   #"./"
+   #"./dashboard/csv_data/final.csv"
 )
-cla_df2 = cla_df[['jc_number','Themes', 'Sub_Themes.1', 'cla_title_en', 'label', 'royal_decree_date',  'toepassing', 'loon', 'duur', 'Summary']]
+cla_df2 = cla_df[['jc_number','Themes', 'Sub_Themes.1', 'cla_title_en', 'royal_decree_date',  'toepassing', 'loon', 'duur', 'Summary']]
 
 # an example based on https://www.ag-grid.com/javascript-data-grid/component-cell-renderer/#simple-cell-renderer-example
 BtnCellRenderer = JsCode('''
@@ -57,43 +53,33 @@ class BtnCellRenderer {
          <span>
             <button id='click-button' 
                 class='btn-simple' 
-                style='color: ${this.params.color}; background-color: ${this.params.background_color}'>Click!</button>
+                style='color: ${this.params.color}; background-color: ${this.params.background_color}'>Summarize</button>
          </span>
       `;
-
         this.eButton = this.eGui.querySelector('#click-button');
-
         this.btnClickedHandler = this.btnClickedHandler.bind(this);
         this.eButton.addEventListener('click', this.btnClickedHandler);
-
     }
-
     getGui() {
         return this.eGui;
     }
-
     refresh() {
         return true;
     }
-
     destroy() {
         if (this.eButton) {
             this.eGui.removeEventListener('click', this.btnClickedHandler);
         }
     }
-
     btnClickedHandler(event) {
-        if (confirm('Are you sure you want to CLICK?') == true) {
-            if(this.params.getValue() == 'clicked') {
-                this.refreshTable('');
-            } else {
-                this.refreshTable('clicked');
-            }
-                console.log(this.params);
-                console.log(this.params.getValue());
-            }
+        if(this.params.getValue() == 'clicked') {
+            this.refreshTable('');
+        } else {
+            this.refreshTable('clicked');
         }
-
+            console.log(this.params);
+            console.log(this.params.getValue());
+        }
     refreshTable(value) {
         this.params.setValue(value);
     }
@@ -102,18 +88,24 @@ class BtnCellRenderer {
 
 df = cla_df2
 gb = GridOptionsBuilder.from_dataframe(df, enableRowGroup=True)
+gb.configure_default_column(editable=True, min_column_width=100)
+gb.configure_column(field='jc_number', header_name='JC Number', supressSizeToFit = 'true')
+gb.configure_column(field = 'Themes', header_name='Theme', surpressSizeToFit = 'true')
+gb.configure_column(field = 'Sub_Themes.1', header_name='Sub Theme', surpressSizeToFit = 'true')
+gb.configure_columns(column_names = ['loon', 'duur', 'toepassing', 'Summary'], hide = 'true' )
 
-gb.configure_default_column(editable=True)
 grid_options = gb.build()
 
 grid_options['columnDefs'].append({
+    #field = column header
     "field": "clicked",
-    "header": "Clicked",
+    "header": "Summarize",
     "cellRenderer": BtnCellRenderer,
     "cellRendererParams": {
-        "color": "red",
-        "background_color": "black",
+        "color": "black",
+        "background_color": "white",
     },
+    'width' : 400
 })
 
 st.title("Interactive CLA Finder")
@@ -127,9 +119,12 @@ response = AgGrid(df,
                   reload_data=False,
                   try_to_convert_back_to_original_types=False
                   )
-
-st.write(response['data'])
+summary_cell = response['data']['Summary']
+#st.write(summary_cell)
 try:
-    st.write(response['data'][response['data'].clicked == 'clicked'])
+    st.write(summary_cell[response['data'].clicked == 'clicked'])
 except:
     st.write('Nothing was clicked')
+#extra code
+#if (confirm('Are you sure you want to CLICK?') == true) {
+#     }
