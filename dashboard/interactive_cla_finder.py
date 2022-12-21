2e54dd1531ca60b67efab1df63045dcc30cd73d5
samuelfooks <samuel.fooks@gmail.com>
2022-12-21 18:31:00 +0100

    dashboard

--- dashboard/interactive_cla_finder.py
+++ dashboard/interactive_cla_finder.py
@@ -2,6 +2,8 @@ import datetime
 import numpy as np
 import pandas as pd
 import streamlit as st
+import urllib
+import base64 
 from st_aggrid import AgGrid, GridOptionsBuilder, JsCode,GridUpdateMode
 now = int(datetime.datetime.now().timestamp())
 start_ts = now - 3 * 30 * 24 * 60 * 60
@@ -41,10 +43,17 @@ cla_df = pd.read_csv(
    "./csv_data/final.csv"
    #"./dashboard/csv_data/final.csv"
 )
-cla_df2 = cla_df[['jc_number','Themes', 'Sub_Themes.1', 'cla_title_en', 'royal_decree_date',  'toepassing', 'loon', 'duur', 'Summary']]
 
+def make_link(filename):
+    link_base = 'https://public-search.werk.belgie.be/website-download-service/joint-work-convention/'
+    JC_num=str(filename)[0:3] + '/'
+    full_link = link_base + str(JC_num)+ str(filename)
+    return full_link
+
+cla_df2 = cla_df[['jc_number','Themes', 'Sub_Themes.1', 'royal_decree_date',  'toepassing', 'loon', 'duur', 'filename', 'Summary']]
+cla_df2['link'] = cla_df2.apply(lambda x : make_link(x['filename']), axis=1 )
 # an example based on https://www.ag-grid.com/javascript-data-grid/component-cell-renderer/#simple-cell-renderer-example
-BtnCellRenderer = JsCode('''
+BtnCellRenderer1 = JsCode('''
 class BtnCellRenderer {
     init(params) {
         this.params = params;
@@ -85,46 +94,116 @@ class BtnCellRenderer {
     }
 };
 ''')
+BtnCellRenderer2 = JsCode('''
+class BtnCellRenderer {
+    init(params) {
+        this.params = params;
+        this.eGui = document.createElement('div');
+        this.eGui.innerHTML = `
+         <span>
+            <button id='click-button' 
+                class='btn-simple' 
+                style='color: ${this.params.color}; background-color: ${this.params.background_color}'>PDF</button>
+         </span>
+      `;
+        this.eButton = this.eGui.querySelector('#click-button');
+        this.btnClickedHandler = this.btnClickedHandler.bind(this);
+        this.eButton.addEventListener('click', this.btnClickedHandler);
+    }
+    getGui() {
+        return this.eGui;
+    }
+    refresh() {
+        return true;
+    }
+    destroy() {
+        if (this.eButton) {
+            this.eGui.removeEventListener('click', this.btnClickedHandler);
+        }
+    }
+    btnClickedHandler(event) {
+        if(this.params.getValue() == 'clicked') {
+            this.refreshTable('');
+        } else {
+            this.refreshTable('clicked');
+        }
+            console.log(this.params);
+            console.log(this.params.getValue());
+        }
+    refreshTable(value) {
+        this.params.setValue(value);
+    }
+};
+''')
 
 df = cla_df2
-gb = GridOptionsBuilder.from_dataframe(df, enableRowGroup=True)
-gb.configure_default_column(editable=True, min_column_width=100)
-gb.configure_column(field='jc_number', header_name='JC Number', supressSizeToFit = 'true')
-gb.configure_column(field = 'Themes', header_name='Theme', surpressSizeToFit = 'true')
-gb.configure_column(field = 'Sub_Themes.1', header_name='Sub Theme', surpressSizeToFit = 'true')
+gb = GridOptionsBuilder.from_dataframe(df, enableRowGroup=True, enableValue=True, enablePivot=True)
+gb.configure_default_column(editable=True)
+gb.configure_column(field='jc_number', header_name='JC Number', supressSizeToFit = 'false')
+gb.configure_column(field = 'Themes', header_name='Theme', surpressSizeToFit = 'false')
+gb.configure_column(field = 'Sub_Themes.1', header_name='Sub Theme', surpressSizeToFit = 'false')
+gb.configure_column(field = 'filename', header_name='File', surpressSizeToFit='false')
+gb.configure_column(field = 'royal_decree_date', header_name= 'Declaration Date', surpressedSizeToFit='false')
+gb.configure_column(field = 'Summary', width = 400)
 gb.configure_columns(column_names = ['loon', 'duur', 'toepassing', 'Summary'], hide = 'true' )
-
+gb.configure_side_bar()
+gb.configure_selection(header_checkbox=True)
 grid_options = gb.build()
 
 grid_options['columnDefs'].append({
     #field = column header
     "field": "clicked",
     "header": "Summarize",
-    "cellRenderer": BtnCellRenderer,
+    "cellRenderer": BtnCellRenderer1,
     "cellRendererParams": {
         "color": "black",
         "background_color": "white",
-    },
-    'width' : 400
-})
+    }
+}
+)
+grid_options['columnDefs'].append({
+    #field = column header
+    "field": "clicked2",
+    "header": "ViewPDF",
+    "cellRenderer": BtnCellRenderer2,
+    "cellRendererParams": {
+        "color": "white",
+        "background_color": "red",
+    }
+}
+)
 
 st.title("Interactive CLA Finder")
 
 response = AgGrid(df,
                   theme="streamlit",
                   key='table1',
+                  enable_enterprise_modules=True,
                   gridOptions=grid_options,
                   allow_unsafe_jscode=True,
                   fit_columns_on_grid_load=True,
                   reload_data=False,
-                  try_to_convert_back_to_original_types=False
+                  try_to_convert_back_to_original_types=False,
+                  width = '100%',
+                  height = 400,           
                   )
 summary_cell = response['data']['Summary']
 #st.write(summary_cell)
 try:
     st.write(summary_cell[response['data'].clicked == 'clicked'])
 except:
-    st.write('Nothing was clicked')
+    st.write('Summary Below')
 #extra code
 #if (confirm('Are you sure you want to CLICK?') == true) {
 #     }
+#function to display the PDF of a given file 
+def displayPDF(file):
+    # Opening file from file path. this is used to open the file from a website rather than local
+    with urllib.request.urlopen(file) as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="950" type="application/pdf"></iframe>'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
